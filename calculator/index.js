419d724f5192dfa285b1b36bc96ee135df2acdce
Naman <namanjaidka89@gmail.com>
2022-10-27 12:34:02 +0530

    Basic Calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,15 +1,29 @@
+// getting all the buttons in one variable
 let buttons = document.querySelectorAll("button");
+
+// getting input in variable named solution
 let solution = document.getElementById("solution");
-// solution.value = 0;
+
 let result = "";
 
+// adding forach method to buttons and passing button in it to get every single button
 buttons.forEach((button) => {
+  // addding addEventListener to button, so when it is clicked it invokes a function with a eveent parameter
   button.addEventListener("click", (e) => {
+    // using conditional statements
+    // e is used to target or get the innerHTML of the buttons
+
     if (e.target.innerHTML === "=") {
+      //result was declared with a empty string as a value
+
       result = eval(result);
+      // so now 2 numbers is add/subtracted/multiplied/divided using eval(function)
+      // for eg eval(1+1) returns 2
       solution.value = result;
+
+      // to clear the input box
     } else if (e.target.innerHTML === "C") {
-      result = 0;
+      result = "";
       solution.value = result;
     } else {
       result = result + e.target.innerHTML;
